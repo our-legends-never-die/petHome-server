17768f501f29927c885de977124cdf26f38cffd6
huangshuai <chimmhuang@163.com>
2021-01-08 21:59:15 +0800

    :rocket: 新增 consul 注册中心

--- petHome-provider/src/main/kotlin/com/github/pethome/provider/ProviderApplication.kt
+++ petHome-provider/src/main/kotlin/com/github/pethome/provider/ProviderApplication.kt
@@ -2,12 +2,24 @@ package com.github.pethome.provider
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * @author Chimm Huang
  */
+@RestController
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
-open class ProviderApplication
+open class ProviderApplication {
+    @GetMapping("/health")
+    fun health(): Boolean {
+        return true
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ProviderApplication>(*args)
