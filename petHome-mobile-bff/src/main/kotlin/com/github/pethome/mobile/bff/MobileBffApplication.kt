3f571553b8644cb15d3340d6c76e850fd992e848
吃茫茫 <chimmhuang@163.com>
2021-01-08 18:06:23 +0800

    :rocket: 新增 consul 注册中心

--- petHome-mobile-bff/src/main/kotlin/com/github/pethome/mobile/bff/MobileBffApplication.kt
+++ petHome-mobile-bff/src/main/kotlin/com/github/pethome/mobile/bff/MobileBffApplication.kt
@@ -2,14 +2,29 @@ package com.github.pethome.mobile.bff
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * @author Chimm Huang
  * @date 2021/01/08
  */
+@RestController
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
-open class MobileBffApplication
+open class MobileBffApplication {
+
+    @GetMapping("/health")
+    fun health(): Boolean {
+        return true
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<MobileBffApplication>(*args)
 }
+
+
